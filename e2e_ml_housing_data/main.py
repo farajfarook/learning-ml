90a7ef63f1fb62744f4408ab06e2c13a23c29e4a
Faraj Farook <farajfarook@gmail.com>
2025-05-17 17:56:57 +1000

    Add Decision Tree Regressor model and enhance evaluation metrics in main.py

--- e2e_ml_housing_data/main.py
+++ e2e_ml_housing_data/main.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import root_mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
+from sklearn.tree import DecisionTreeRegressor
 
 from load_data import load_housing_data
 from preprocessor import create_preprocessor
@@ -25,14 +27,25 @@ test_set.drop("income_cat", axis=1, inplace=True)
 housing = train_set.drop("median_house_value", axis=1)
 housing_labels = train_set["median_house_value"].copy()
 
-# Create a pipeline for preprocessing and model training
+# Linear Regression
 preprocessor = create_preprocessor()
 lin_reg = make_pipeline(preprocessor, LinearRegression())
-
-# Fit the model
 lin_reg.fit(housing, housing_labels)
 
 # Evaluate the model
 housing_predictions = lin_reg.predict(housing)
 print("Predictions:", housing_predictions[:5].round(2))
 print("Labels:", housing_labels.iloc[:5].values)
+lin_rmse = root_mean_squared_error(housing_labels, housing_predictions)
+print("Root Mean Squared Error:", lin_rmse)
+
+# Decision Tree Regressor
+tree_reg = make_pipeline(preprocessor, DecisionTreeRegressor())
+tree_reg.fit(housing, housing_labels)
+
+# Evaluate the model
+housing_predictions = tree_reg.predict(housing)
+print("Predictions:", housing_predictions[:5].round(2))
+print("Labels:", housing_labels.iloc[:5].values)
+tree_rmse = root_mean_squared_error(housing_labels, housing_predictions)
+print("Root Mean Squared Error:", tree_rmse)
