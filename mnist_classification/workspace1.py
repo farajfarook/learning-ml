4beb1d3aa3068cbd97f5f597034ae69940b8695a
Faraj Farook <farajfarook@gmail.com>
2025-05-18 22:19:44 +1000

    Add SGDClassifier implementation for binary classification of digit '5'

--- mnist_classification/workspace1.py
+++ mnist_classification/workspace1.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 from sklearn.datasets import fetch_openml
+from sklearn.linear_model import SGDClassifier
 
 from plot_digit import plot_digit
 
@@ -13,8 +14,23 @@ X, y = mnist["data"], mnist["target"]
 
 x_train, x_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 
-for i in range(100):
-    plt.subplot(10, 10, i + 1)
-    plot_digit(x_train[i])
+# for i in range(100):
+#    plt.subplot(10, 10, i + 1)
+#    plot_digit(x_train[i])
+#
+# plt.show()
 
-plt.show()
+y_train_5 = y_train == "5"
+y_test_5 = y_test == "5"
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(x_train, y_train_5)
+
+
+# for i in range(9):
+#    plt.subplot(3, 3, i + 1)
+#    value = sgd_clf.predict([x_train[i]])
+#    plt.title(f"{value[0]}")
+#    plot_digit(x_train[i])
+#
+# plt.show()
