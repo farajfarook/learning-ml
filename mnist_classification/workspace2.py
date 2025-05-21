70910727f3fca7622ebb48cfbd2d901c79f0cb47
Faraj Farook <farajfarook@gmail.com>
2025-05-21 23:23:10 +1000

    Refactor SVC prediction code for clarity and add decision function score output

--- mnist_classification/workspace2.py
+++ mnist_classification/workspace2.py
@@ -10,5 +10,12 @@ x_train, x_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 svc_clf = SVC(random_state=42)
 svc_clf.fit(x_train[:2000], y_train[:2000])
 
-svc_pred = svc_clf.predict([x_train[4]])
+some_digit = x_train[4]
+svc_pred = svc_clf.predict([some_digit])
 print("SVC prediction: ", svc_pred)
+
+some_digit_score = svc_clf.decision_function([some_digit])
+print("SVC decision function score: ", some_digit_score)
+
+class_id = some_digit_score.argmax()
+print("Class ID: ", class_id)
